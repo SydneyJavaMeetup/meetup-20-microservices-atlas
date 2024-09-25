c4e8a156f52d198522ca128e250cb63ff39d33bf
Luke Thompson <endeavour9@gmail.com>
2024-09-25 12:29:24 +1000

    Update create-index.js

--- sample-data/create-index.js
+++ sample-data/create-index.js
@@ -1,3 +1,4 @@
+db.photo.remove({colours: {$nin: ['Black', 'Brown', 'Golden', 'Grey', 'White']}});
 db.getSiblingDB('AtlasSearch').getCollection('photo').createSearchIndex('default',
 INDEX_GOES_HERE
 );
